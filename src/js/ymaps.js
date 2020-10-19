d78406df5d664a135904ec1f1785d8d9466cdbba
Denis-Str <web_life@inbox.ru>
2020-10-19 16:11:52 +0300

    доделал ДЗ

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,52 +1,66 @@
 import popup from '../templates/popup.hbs';
 
 function mapInit() {
-  let placemarkData = [];
+  let placemarks = [];
 
   ymaps.ready(() => {
-        let map = new ymaps.Map('map', {
-            center: [47.222078, 39.720349],
-            zoom: 12,
-            controls: ['zoomControl'],
-            behaviors: ['drag']
-        });
+    let myMap = new ymaps.Map('map', {
+      center: [47.222078, 39.720349],
+      zoom: 12,
+      controls: ['zoomControl'],
+      behaviors: ['drag']
+    });
 
-        map.events.add('click', (e) => createdBalloon(e.get('coords')));
+    const clusterer = new ymaps.Clusterer({
+      "preset": "islands#invertedVioletClusterIcons",
+      "groupByCoordinates": true,
+      "clusterDisableClickZoom": true,
+      "hasBalloon": false
+    });
+    myMap.geoObjects.add(clusterer);
 
-         function createdBalloon(coords) {
-           map.balloon.open(coords, { content: popup() });
-           document.addEventListener('click', (e) => {
-
-             if (e.target.id === 'btn') {
-               let name = document.querySelector('#name').value
-               let place = document.querySelector('#place').value
-               let content = document.querySelector('#content').value
-
-               placemarkData.push({ coords, info: { name, place, content }})
-               createdPlacemark(coords, placemarkData)
-               map.balloon.close();
-             }
-           });
-         }
-
-        function createdPlacemark(coords, placemarkData) {
-          //console.log(placemarkData)
-          // let data = [
-          //   {
-          //     coords: [1,2],
-          //     info: {name: "1", place: "2", content: "3"}
-          //   },
-          //   {
-          //     coords: [1,2],
-          //     info: {name: "5", place: "6", content: "7"}
-          //   }
-          // ]
-          let myPlacemark = new ymaps.Placemark(coords, {
-            balloonContentBody: popup({placemarkData})
-          });
-          map.geoObjects.add(myPlacemark);
-        }
+    clusterer.events.add('click', e => {
+      let coords = e.get('target').geometry.getCoordinates();
+      if (e.get('target').options.getName() === 'cluster') createdBalloon(coords);
     })
+
+    myMap.events.add('click', (e) => createdBalloon(e.get('coords')));
+
+    document.addEventListener('click', (e) => {
+
+      if (e.target.dataset.btn === 'add') {
+        let coordsStr = document.querySelector('[data-info]').dataset.info;
+        let coords = JSON.parse(coordsStr);
+        let name = document.querySelector('#name').value;
+        let place = document.querySelector('#place').value;
+        let content = document.querySelector('#content').value;
+        placemarks.push({coords: coordsStr, name, place, content});
+        createdPlacemark({coords, info: {name, place, content}});
+        myMap.balloon.close();
+      }
+    });
+
+    function createdBalloon(coords) {
+      myMap.balloon.open(coords, popup({
+        coords: JSON.stringify(coords),
+        placemarks: getInfoByCoords(coords)
+      }));
+    }
+
+    function createdPlacemark({coords}) {
+      let myPlacemark = new ymaps.Placemark(coords, {
+        balloonContentBody: popup({
+          coords: JSON.stringify(coords),
+          placemarks: getInfoByCoords(coords)
+        })
+      });
+      clusterer.add(myPlacemark);
+    }
+
+    function getInfoByCoords(coords) {
+      return placemarks.filter((pin) => pin.coords === JSON.stringify(coords))
+    }
+  })
 }
 
-export { mapInit }
+export {mapInit}
